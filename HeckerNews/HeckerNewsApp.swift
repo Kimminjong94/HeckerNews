ddb4313feef79b0da859f6fab883324c6fb03691
minjongkim <rlaalswhd200@naver.com>
2021-11-08 17:23:37 +0900

    Initial Commit

---
+++ HeckerNews/HeckerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeckerNewsApp.swift
+//  HeckerNews
+//
+//  Created by 김민종 on 2021/11/08.
+//
+
+import SwiftUI
+
+@main
+struct HeckerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
